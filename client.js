0ee6f1d0059818e27dd6f475a85d23d3808bda14
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-19 05:08:04 +0000

    Updated the file to add comments at the top describing what the file is currently doing

--- client.js
+++ client.js
@@ -1,3 +1,10 @@
+// 1.   Project Name: Snake
+// 2.   File Name: client.js
+// 3.   Written By: Brad Sawyer
+// 4.   Date: 18th August, 2021
+// 5.   Purpose: This file current holds the function that connects with the game server. 
+//                The function is exported so it's available in other files.
+
 const net = require('net');
 
 //Esablishes connection with the game server
