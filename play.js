bea73d6d9a8d6a444fe09731e9e5c5dbeeb21f03
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-19 05:07:27 +0000

    Updated the comments in the file

--- play.js
+++ play.js
@@ -1,7 +1,8 @@
 // 1.   Project Name: Snake
-// 2.   Written By: Brad Sawyer
-// 3.   Date: 18th August, 2021
-// 4.   Purpose: This project will give the ability to work with on our skills of TCP while 
+// 2.   File Name: play.js
+// 3.   Written By: Brad Sawyer
+// 4.   Date: 18th August, 2021
+// 5.   Purpose: This project will give the ability to work with on our skills of TCP while 
 //                also giving the chance to continue practicing with FOCAL in a fun way by
 //                implementing the game Snake. This will be the main file for the entire 
 //                project. There may be other files for modularizing the code.
