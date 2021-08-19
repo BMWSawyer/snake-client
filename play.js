000515333907feb0ad302a6f4aa01ca1491c40a7
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-19 04:59:12 +0000

    Added the functionality to receive messages from the game server

--- play.js
+++ play.js
@@ -11,12 +11,17 @@ const net = require('net');
 //Esablishes connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '10.0.0.209',
+    host: 'localhost',
     port: 50541
   });
 
   //interprets the incoming data as text
   conn.setEncoding('utf8');
+
+  //implements the ability to receive messages from the game server
+  conn.on('data', (message) => {
+    console.log(`Message from the server: ${message}`);
+  });
 
   return conn;
 }
