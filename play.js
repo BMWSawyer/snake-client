5d36fbede8f5a444c36968a29d14f9b80a0b2efc
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-20 19:39:16 +0000

    Updated the file by removing the function handling the user input from the keyboard and moving it to it's own file. Required that function from it's file within this file

--- play.js
+++ play.js
@@ -8,26 +8,10 @@
 //                project. There may be other files for modularizing the code.
 
 //Requires the connect function to be able to connect with the game server
-const connect = require('./client');
+const { connect } = require('./client');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-
-  stdin.resume();
-  return stdin;
-}
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
+//Requires the setupInput function to be able to handle user input from the keyboard
+const { setupInput } = require('./input');
 
 console.log('Connecting...');
 connect();
