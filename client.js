f54620414504e31be03b89e554acfcc6ae5cc6ea
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-20 16:39:14 +0000

    Added an event handler to send a message to print a message to the player indicating the successful connection to the server

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // 1.   Project Name: Snake
 // 2.   File Name: client.js
 // 3.   Written By: Brad Sawyer
-// 4.   Date: 18th August, 2021
+// 4.   Date Started: 18th August, 2021
 // 5.   Purpose: This file current holds the function that connects with the game server. 
 //                The function is exported so it's available in other files.
 
@@ -17,9 +17,13 @@ const connect = function () {
   //Interprets the incoming data as text
   conn.setEncoding('utf8');
 
-  //implements the ability to receive messages from the game server
+  //Implements the ability to receive messages from the game server
   conn.on('data', (message) => {
     console.log(`Message from the server: ${message}`);
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to the game server')
   });
 
   return conn;
