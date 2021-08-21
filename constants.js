f7366346c7765604063ad4ecb3fc44ffd5c99ec5
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-21 05:21:51 +0000

    Refactored and dried up existing code. Also checked the linting of the code as well.

--- constants.js
+++ constants.js
@@ -2,11 +2,10 @@
 // 2.   File Name: constants.js
 // 3.   Written By: Brad Sawyer
 // 4.   Date Started: 20th August, 2021
-// 5.   Purpose: This file holds all of the constant values within the program and exports 
+// 5.   Purpose: This file holds all of the constant values within the program and exports
 //                them to be used accordingly in other files as needed.
 
 const IP = 'localhost';
-
 const PORT = 50541;
 
 
