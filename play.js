2aaa69bbc931976072e38a9d026311790366c6d1
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-21 05:20:52 +0000

    Refactored and dried up existing code. Also checked the linting of the code as well.

--- play.js
+++ play.js
@@ -2,9 +2,9 @@
 // 2.   File Name: play.js
 // 3.   Written By: Brad Sawyer
 // 4.   Date Started: 18th August, 2021
-// 5.   Purpose: This project will give the ability to work with on our skills of TCP while 
+// 5.   Purpose: This project will give the ability to work with on our skills of TCP while
 //                also giving the chance to continue practicing with FOCAL in a fun way by
-//                implementing the game Snake. This will be the main file for the entire 
+//                implementing the game Snake. This will be the main file for the entire
 //                project. There may be other files for modularizing the code.
 
 //Requires the connect function to be able to connect with the game server
