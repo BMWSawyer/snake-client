cfdfd9b9bf1e6d3cd53e6ff066e359333fa27334
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-21 05:21:17 +0000

    Refactored and dried up existing code. Also checked the linting of the code as well.

--- input.js
+++ input.js
@@ -2,7 +2,7 @@
 // 2.   File Name: input.js
 // 3.   Written By: Brad Sawyer
 // 4.   Date Started: 20th August, 2021
-// 5.   Purpose: This file handles the user input from the keyboard in order to be able to 
+// 5.   Purpose: This file handles the user input from the keyboard in order to be able to
 //                move the snake during the game or exit the game.
 
 
@@ -16,39 +16,38 @@ const setupInput = function(conn) {
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
+  stdin.on('data', (key) => handleUserInput(key));
 
   return stdin;
-}
+};
 
 //Handles user input from the keyboard. Either exits game or sends data back to the setupInput function
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
+  switch (key) {
+  case 'w':
+    connection.write('Move: up');
+    break;
+  case 'a':
+    connection.write('Move: left');
+    break;
+  case 's':
+    connection.write('Move: down');
+    break;
+  case 'd':
+    connection.write('Move: right');
+    break;
+  case 'b':
+    connection.write('Say: Gotcha!');
+    break;
+  case 'h':
+    connection.write('Say: You got me!');
+    break;
+  case 'n':
+    connection.write('Say: I\'m coming for you!');
+    break;
+  case '\u0003':
     process.exit();
   }
-  if (key === 'w') {
-    connection.write('Move: up');
-  }
-  if (key === 's') {
-    connection.write('Move: down');
-  }
-  if (key === 'a') {
-    connection.write('Move: left');
-  }
-  if (key === 'd') {
-    connection.write('Move: right');
-  }
-  if (key === 'b') {
-    connection.write('Say: Gotcha!');
-  }
-  if (key === 'h') {
-    connection.write('Say: You got me!');
-  }
-  if (key === 'n') {
-    connection.write('Say: I\'m coming for you!');
-  }
-}
+};
 
 module.exports = { setupInput };
