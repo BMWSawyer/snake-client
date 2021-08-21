0b8c51f7567eb972a8598e21f72129994d4a77ec
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-21 05:21:33 +0000

    Refactored and dried up existing code. Also checked the linting of the code as well.

--- client.js
+++ client.js
@@ -2,14 +2,14 @@
 // 2.   File Name: client.js
 // 3.   Written By: Brad Sawyer
 // 4.   Date Started: 18th August, 2021
-// 5.   Purpose: This file current holds the function that connects with the game server. 
+// 5.   Purpose: This file current holds the function that connects with the game server.
 //                The function is exported so it's available in other files.
 
 const net = require('net');
 const { IP, PORT } = require('./constants');
 
 //Esablishes connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: IP,
     port: PORT
@@ -19,22 +19,16 @@ const connect = function () {
   conn.setEncoding('utf8');
 
   //Implements the ability to receive messages from the game server
-  conn.on('data', (message) => {
-    console.log(`Message from the server: ${message}`);
-  });
+  conn.on('data', (message) => console.log(`Message from the server: ${message}`));
 
   //Prints message to the client upon connection with server
-  conn.on('connect', () => {
-    console.log('Successfully connected to the game server')
-  });
+  conn.on('connect', () => console.log('Successfully connected to the game server'));
 
   //Sends a name back to the server in the form of a three character alpha-numeric initial
-  conn.on('connect', () => {
-    conn.write("Name: BMW");
-  });
+  conn.on('connect', () => conn.write("Name: BMW"));
 
   return conn;
-}
+};
 
 //Exports the function to be used in other files
 module.exports = { connect };
