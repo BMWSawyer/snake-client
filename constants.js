192a7c9cbd8e2c6c37447b7adc652aa46b12d5f7
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-21 02:40:35 +0000

    Created the file to hold all of the constant values within the program and export them to be available in files as needed

---
+++ constants.js
@@ -0,0 +1,16 @@
+// 1.   Project Name: Snake
+// 2.   File Name: constants.js
+// 3.   Written By: Brad Sawyer
+// 4.   Date Started: 20th August, 2021
+// 5.   Purpose: This file holds all of the constant values within the program and exports 
+//                them to be used accordingly in other files as needed.
+
+const IP = 'localhost';
+
+const PORT = 50541;
+
+
+module.exports = {
+  IP,
+  PORT
+};
